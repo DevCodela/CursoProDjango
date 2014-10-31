a04fa84460888628c36b15e7a5b2c297f9c5051a
Victor Villazon <victorzn23@gmail.com>
2014-10-31 15:40:25 -0500

    Proyecto: Archivos estáticos y media

--- eventus/eventus/urls.py
+++ eventus/eventus/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.conf import settings
 
 urlpatterns = patterns('',
     # Examples:
@@ -8,3 +9,10 @@ urlpatterns = patterns('',
     url(r'^', include('apps.events.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
+
+if settings.DEBUG:
+    urlpatterns += patterns("",
+        url(r'^media/(?P<path>.*)$', 'django.views.static.serve',
+            {'document_root': settings.MEDIA_ROOT,}
+        ),
+    )
