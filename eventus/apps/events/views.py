98b2c6e89ac258dbed7c0c3dc8093c5056a0d21b
Victor Villazon <victorzn23@gmail.com>
2014-10-31 04:16:59 -0500

    Proyecto: Mi primer template

--- eventus/apps/events/views.py
+++ eventus/apps/events/views.py
@@ -5,5 +5,4 @@ from .models import Event, Category
 def index(request):
 	events = Event.objects.all().order_by('-created')[:6]
 	categories = Category.objects.all()
-	return render(request, 'index.html', {'events' : events,
-				'categories':categories})
+	return render(request, 'events/index.html', {'events' : events, 'categories':categories})
