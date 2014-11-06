6b2cb7d5ad370e22f43a2da7679df8b6664bdd2f
Victor Villazon <victorzn23@gmail.com>
2014-11-06 14:41:10 -0500

    Mejoras en admin.py para el modelo Event

--- eventus/apps/events/admin.py
+++ eventus/apps/events/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from .models import Event, Category, Assistant, Comments
 
-admin.site.register(Event)
+@admin.register(Event)
+class EventAdmin(admin.ModelAdmin):
+	list_display = ('name','category','start','finish','is_free',)
+
 admin.site.register(Category)
 admin.site.register(Assistant)
 admin.site.register(Comments)
