f61ff7219c8419180d81d9d31d91bf0e9b932b97
jean <jean_carlos104@hotmail.com>
2014-11-20 00:28:59 -0500

    a

--- eventus/eventus/settings/prod.py
+++ eventus/eventus/settings/prod.py
@@ -21,8 +21,8 @@ INSTALLED_APPS = INSTALLED_APPS + (
 
 AWS_STORAGE_BUCKET_NAME = 'cursoprodjango'
 STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
-AWS_ACCESS_KEY_ID = 'AKIAJ3J5C2UG3C2H6OYQ'
-AWS_SECRET_ACCESS_KEY = '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'
+AWS_ACCESS_KEY_ID = 'xxxxxxxxxxx'
+AWS_SECRET_ACCESS_KEY = 'yyyyyyyyyyyyyy'
 
 STATIC_URL = 'https://s3.amazonaws.com/cursoprodjango/'
 MEDIA_URL = 'https://s3.amazonaws.com/cursoprodjango/'
